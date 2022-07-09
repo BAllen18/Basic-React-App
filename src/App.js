53d5fba8fcc3015468b82cc3d9dec04f75e71a77
Brooke Allen <brooke.gatewood@outlook.com>
2022-07-09 13:20:39 -0400

    Built a basic app that updated the logo to a kitty. This also allowed a user to click 5 times, and on the 5th time have their count reset. This used state and some basic javascript.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
+import logo from './cat.jpg';
 import './App.css';
 
 function App() {
   // useState keeps an internal state in the component
   const [count, setCount] = useState(0);
+
+function backToZero(){
+  if (count >= 5) {
+    setCount(0);
+    return;
+  }
+  setCount(count+1);
+}
+
 
 
   return (
@@ -12,8 +21,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>You clicked {count} times</p>
-        <button onClick={() => setCount(count + 1)}>
+        <button onClick={() => backToZero()}>
           Click me
+
         </button>
       </header>
     </div>
@@ -21,3 +31,11 @@ function App() {
 }
 
 export default App;
+//kitty logo
+//count by 1 and every 5th click count back 5, 01230 etc
+
+//if(count > 4){
+// count = 0
+
+//}
+//text filed above or beside button inside user enter number 1-10 then genrate a random number based on what they chose (ex 1 = 1-9, 2 =10-99 etc)
